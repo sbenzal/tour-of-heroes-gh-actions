144068b15b73356416a6b22c81d231b60259ce0b
0GiS0 <giselatb@outlook.com>
2024-02-06 08:17:53 +0000

    Add Hero creation and return in root endpoint

--- Program.cs
+++ Program.cs
@@ -1,6 +1,17 @@
+using tour_of_heroes_nuget;
+
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+
+app.MapGet("/", () =>
+{
+    // Create a Hero
+    var hero = new Hero { Id = 1, Name = "Windstorm" };
+
+    // Return the Hero
+    return Results.Ok(hero);
+
+});
 
 app.Run();
