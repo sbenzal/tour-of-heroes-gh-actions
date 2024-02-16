346da353be56cbfc829dbc5482bea321ccebcd9c
0GiS0 <giselatb@outlook.com>
2024-02-16 11:37:30 +0100

    Update self-hosted runner workflow and package version

--- Program.cs
+++ Program.cs
@@ -1,4 +1,4 @@
-// using tour_of_heroes_nuget;
+using tour_of_heroes_nuget;
 
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
@@ -6,13 +6,13 @@ var app = builder.Build();
 
 app.MapGet("/", () =>
 {
-    // // Create a Hero
-    // var hero = new Hero { Id = 1, Name = "Windstorm" };
+    // Create a Hero
+    var hero = new Hero { Id = 1, Name = "Windstorm" };
 
-    // // Return the Hero
-    // return Results.Ok(hero);
+    // Return the Hero
+    return Results.Ok(hero);
 
-    return Results.Ok("Hello World");
+    // return Results.Ok("Hello World");
 
 });
 
